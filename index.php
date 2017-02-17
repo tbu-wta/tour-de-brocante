566ccc255e1eb618274240070934978d6e28f470
tbu <tobias.buchinger@iw.htwchur.ch>
2017-02-17 22:25:44 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+	<html lang="en">
+
+		<head>
+			<!-- set page properties -->
+				<!-- MUST COME FIRST IN HEAD -->
+
+				<!-- set the correct CHARSET -->
+				<meta charset="utf-8">
+
+				<!-- ensure compability with IE -->
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+				<!-- set VIEWPOINT to scale content to the device size and set INITIAL ZOOM level to 1  -->
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<!-- embed custom fonts -->
+			
+				<!-- google fonts -->
+				<!-- <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700|Source+Sans+Pro:300" rel="stylesheet"> -->
+
+				<!-- local fonts -->
+
+			<!-- embed browser tab icon -->
+
+				<!-- my browser tab icon -->
+				<link rel="icon" href="images/MY-ICON.ico">
+
+			<!-- embed CSS [choose local or MaxCND] -->
+
+				<!-- MaxCND: Latest compiled and minified Bootstrap stylesheet -->
+				<!-- W3C says, that MaxCND can improve page load performance -->
+				<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+
+				<!-- Local: Current local Bootstrap stylesheet-->
+				<link rel="stylesheet" href="css/bootstrap.min.css">
+
+				<!-- Local: Our own stylesheets [must be extended] -->
+				<link rel="stylesheet" href="css/custom-styles.css">
+
+			<!-- embed some descriptive META tags -->
+				<meta name="description" content="Guide to second hand stores in Bern.">
+				<meta name="keywords" content="Borckante, Brocki, Brockenstube, Brockenhaus, Bern" >
+				<meta name="author" content="Buchinger, Lehmann, Wanko">
+				<meta name="date created" content="2017.02.17">
+
+			<title> tdb home </title>
+		
+		</head>
+
+		<body>
+
+		<!-- page header -->
+
+		<!-- nav tabs -->
+
+		<!-- contentX -->
+
+		<!-- footer -->
+
+			<div class="container">
+				<div class="row">
+
+
+					<div class="col-sm-9">
+
+					<h1>col-sm-9</h1>
+						
+					</div>
+					<div class="col-sm-3">
+						
+						<h1>col-sm-3</h1>
+
+					</div>
+				</div>
+				
+			</div>
+
+			<!-- load Bootstrap and JQuery [choose local or MaxCND] -->
+				<!-- Bootstrap suggests, to load all scripts at the end of the body tag, to improve page load performance -->
+
+				<!-- W3C says, that MaxCND can improve page load performance -->
+
+				<!-- MaxCND: Latest compiled JQuery JS library -->
+				<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
+
+				<!-- MaxCND: Latest and minified Bootstrap JS library -->
+				<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+
+				<!-- Local: Current local JQuery JS library -->
+				<script src="js/jquery.min.js"></script>
+
+				<!-- Local: Current local Bootstrap JS library -->
+				<script src="js/bootstrap.min.js"></script>
+
+				<!-- Local: Our own JS scripts [must be extended] -->
+				<script src="js/custom-script.js"></script>
+
+		</body>
+
+	</html> 
